6baf8d3693abf8d3bd547dd7162a539970d6ea1c
Daniel San Pedro <156046651+daniel-sanpedro@users.noreply.github.com>
2024-01-29 19:55:38 -0800

    Add files via upload

---
+++ node.js
@@ -0,0 +1,18 @@
+console.log(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+);
+
+var num = 2 + 8;
+console.log(num);
+
+var num = 80 / 2;
+console.log(num);
+
+var num = 13 * 3;
+console.log(num);
+
+let str1 =
+  "You have received this message because you have been chosen to open an important vault.";
+let str2 = "The combination of the lock is";
+let combination = "solved in console";
+alert(str1 + " " + str2 + " " + combination);
